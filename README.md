d99f0dc30548d97c9c5ce8d7492ca56146f3ba94
guolei <174000902@qq.com>
2024-07-11 13:12:52 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # guolei_py3_qunjielong
-guolei_py3_qunjielong
+
+## introduce
+
+**guolei python3 qunjielong library**
+
+## software architecture
+
+~python 3.*
+
+## installation tutorial
+
+```shell
+pip install guolei-py3-qunjielong
+```
+
+## catalog description
+### Qujielong Api Example
+```python
+
+# @see https://console-docs.apipost.cn/preview/b4e4577f34cac87a/1b45a97352d07e60/
+
+from diskcache import Cache
+
+from guolei_py3_qunjielong import Api as QunjielongApi
+
+qujielong_api = QunjielongApi(
+    base_url="https://openapi.qunjielong.com",
+    secret="secret",
+    diskcache=Cache()
+)
+act_goods = qujielong_api.access_token_with_cache().query_act_goods(act_no="2404110172842728")
+print(act_goods)
+```
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(
+    name="guolei-py3-qunjielong",
+    version="0.0.1",
+    description="a python3 qunjielong library by guolei",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/guolei19850528/guolei_py3_qunjielong",
+    author="guolei",
+    author_email="174000902@qq.com",
+    license="MIT",
+    keywors=["qunjielong", "群接龙"],
+    packages=setuptools.find_packages('./'),
+    install_requires=[
+        "guolei-py3-requests",
+        "redis",
+        "diskcache"
+    ],
+    python_requires='>=3.0',
+    zip_safe=False
+)
