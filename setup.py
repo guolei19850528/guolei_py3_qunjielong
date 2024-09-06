0f73eefe97f4df9d50ec6f4d426676e2e097931a
guolei <174000902@qq.com>
2024-09-06 14:10:39 +0800

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setup(
     name="guolei-py3-qunjielong",
-    version="1.0.1",
+    version="1.0.2",
     description="群接龙 API",
     long_description=long_description,
     long_description_content_type="text/markdown",
